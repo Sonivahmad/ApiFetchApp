48738d6bdf8dec0a7ccb6752b1f6b12bfcf184a2
soniv ahmad <sonivahmad@gmail.com>
2022-09-11 17:03:30 +0530

    Projects

--- app.js
+++ app.js
@@ -10,3 +10,9 @@ mealList.addEventListener('click', getMealRecipe);
 recipeCloseBtn.addEventListener('click', () => {
     mealDetailsContent.parentElement.classList.remove('showRecipe');
 });
+
+// get meal list that matches with the ingredients
+function getMealList(){
+    let searchInputTxt = document.getElementById('search-input').value.trim();
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
+   
